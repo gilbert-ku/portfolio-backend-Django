9163e41012593a64e68812a6b80d74a3fa391397
gilbert-ku <gilbert45ku@gmail.com>
2024-07-15 23:42:52 +0300

    add contact models fields

--- app/settings.py
+++ app/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "phonenumber_field",
 
     # MY APPS
-    'account',
+    # 'account',
     "contacts",
 ]
 
--- contacts/models.py
+++ contacts/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
+
 
 # Create conact model
 
 class Contacts(models.Models):
     user = models.ForeignKey(
-        User, on_delete= models.CASCADE, null=False, blank=True
+        User, on_delete= models.CASCADE, null=False, blank=False
     )
 
     full_name = models.CharField(max_length=100)
-    # phone_number = models.(max_length=100)
+    phone = PhoneNumberField()
+    email = models.EmailField(max_length=150)
+    subject = models.CharField(max_length=200)
+    message = models.TextField(max_length=500)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
+    
+    # class Meta:
+    #     order_with_respect_to = "user"
+
