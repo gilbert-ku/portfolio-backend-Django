d85f3df2f0f9583e6758a5e3c3615dd8ac79a1e1
gilbert-ku <gilbert45ku@gmail.com>
2024-07-18 23:11:55 +0300

    update projects modes

--- projects/models.py
+++ projects/models.py
@@ -1,16 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import uuid
+
 
 # Create conact model
 
-class Projects(models.Model):
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    created_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now_add=True)
+
+
+    class Meta:
+        abstract = True
+
+
+
+class Projects(BaseModel):
     user = models.ForeignKey(
         User, on_delete= models.CASCADE, null=False, blank=False
     )
 
     title = models.CharField(max_length=100)
-    image = models.ImageField()
+    image = models.ImageField(upload_to= "projects")
     desctiption = models.TextField(max_length=500)
     url_link = models.URLField()
     date = models.DateTimeField(auto_now_add=True)
