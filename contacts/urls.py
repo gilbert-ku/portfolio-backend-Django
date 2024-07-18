438eefd908188911ed3745ca984e838a502487bb
gilbert-ku <gilbert45ku@gmail.com>
2024-07-18 22:43:52 +0300

    update urls

--- contacts/urls.py
+++ contacts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from contacts.views import IndexView, ContactView
 
 urlpatterns = [
-    path("index/", IndexView.as_view()),
+    path("", IndexView.as_view()),
     path("contact/", ContactView.as_view()),
     path('contact/<int:id>/', ContactView.as_view()),
 ]
