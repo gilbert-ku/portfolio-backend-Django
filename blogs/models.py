4f8d0281626ed2c84a851c00c985213e940666f0
gilbert-ku <gilbert45ku@gmail.com>
2024-07-22 22:37:15 +0300

    add blog models unique id

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+import uuid  #is used to generate uniq id
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(primary_key= True, editable=False, default=uuid.uuid4)
+
+    created_date = models.DateField(auto_now_add=True)
+    created_time = models.TimeField(auto_now_add=True)
+    updated_date = models.DateField(auto_now_add=True)
+    updated_time = models.TimeField(auto_now_add=True)
+
+
+    class Meta:
+        abstract = True
+
+
