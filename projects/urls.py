c2f85c6ee2818faeda08ff8d5a036f12383de8d2
gilbert-ku <gilbert45ku@gmail.com>
2024-07-22 15:41:37 +0300

    add post request for projects

--- projects/urls.py
+++ projects/urls.py
@@ -4,7 +4,7 @@ from projects.views import ProjectsView, ProjectIndex
 
 urlpatterns = [
     path("", ProjectIndex.as_view()),
-    path("Projects", ProjectsView.as_view()),
+    path("Projects/", ProjectsView.as_view()),
     path('project/<int:id>/', ProjectsView.as_view()),
 ]
 
--- projects/views.py
+++ projects/views.py
@@ -11,5 +11,31 @@ class ProjectIndex(APIView):
         return Response({"message": "welcome to first django api"}) 
     
 class ProjectsView(APIView):
-    def get(self, request):
-        return Response({"message": "welcome to first django api"})
+    # def get(self, request):
+    #     return Response({"message": "welcome to first django api"})
+
+    def post(self, request):
+        try:
+            data = request.data
+
+            serializer = ProjectSerializer(data = data)
+
+            if not serializer.is_valid():   
+                return Response({
+                    "data" : serializer.errors,
+                    "message" : "something went wrong"
+                }, status= status.HTTP_400_BAD_REQUEST)
+            
+            serializer.save()
+
+            return Response({
+                "data" : {},
+                "message" : "Message Sent successfully"
+            }, status =  status.HTTP_201_CREATED)
+        
+
+        except Exception as e:
+            return Response({
+                "data" : {},
+                "message" : "something went wrong"
+            }, status =  status.HTTP_400_BAD_REQUEST)
