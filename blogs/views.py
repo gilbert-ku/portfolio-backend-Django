50eed35012e4d9521158fc31e138ce1031c29ccd
gilbert-ku <gilbert45ku@gmail.com>
2024-07-22 23:19:27 +0300

    add get request for blogindex

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializer import BlogsSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Blogs
 
 # Create your views here.
+
+
+class BlogsIndex(APIView):
+    def get(self, request):
+        return Response({"message": "welcome to my blogs"})
