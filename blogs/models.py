e2bc33d73e17660b9a6ffe4aa9eeeabcd7746c5c
gilbert-ku <gilbert45ku@gmail.com>
2024-07-22 23:11:43 +0300

    add blog model

--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 import uuid  #is used to generate uniq id
@@ -15,4 +16,16 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
+class Blogs(BaseModel):
+    user = models.ForeignKey(
+        User, on_delete= models.CASCADE, null=False, blank=False
+    )
 
+    title = models.CharField(max_length=150)
+    image = models.ImageField(upload_to= "blog/blogpost")
+    blog_text = models.TextField(max_length=3000)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
