4dea9bd410cc48cfe49991ac1b5d8a002e0f3708
gilbert-ku <gilbert45ku@gmail.com>
2024-08-08 00:05:07 +0300

    add CORS

--- app/settings.py
+++ app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # third party app
     'rest_framework',
     "phonenumber_field",
+    # "corsheaders"
     # "pillow"
 
     # MY APPS
@@ -51,6 +52,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +135,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
