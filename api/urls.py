acc1f14d7167a832d35e6d882bf4a8feb6e44745
gilbert-ku <gilbert45ku@gmail.com>
2024-08-18 00:55:10 +0300

    update api contact urls

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('Contacts/', include("contacts.urls")),
+    path("", include("contacts.urls")),
     path("", include("projects.urls")),
     path("", include("blogs.urls")),
     path("", include("comments.urls"))
