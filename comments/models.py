a12e9ffff265322dcbbfa4037ba414129420b731
gilbert-ku <gilbert45ku@gmail.com>
2024-08-17 23:00:57 +0300

    add comments model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from ..projects import Projects
+
+import uuid
 
 
+class BaseModel(models.Model):
+
+    # create prmary key
+    uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+
+    #  add tine stamp
+    created_date = models.DateField(auto_now_add=True)
+    created_time = models.TimeField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+    updated_time = models.TimeField(auto_now=True)
+
+
+    class Meta:
+        abstract = True
+
+class Comments(models.Model):
+    # foriegn key
+    Projects = models.ForeignKey(
+        Projects, on_delete= models.CASCADE, null=False, blank= False
+    )
+
+    # fields
+    comments = models.TextField(max_length=1000, null=True, blank=True)
+    like = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.comments
