3825188d4c4f906bd07a2ae06634884e2ac89354
gilbert-ku <gilbert45ku@gmail.com>
2024-08-22 21:42:54 +0300

    update database config

--- app/settings.py
+++ app/settings.py
@@ -103,14 +103,26 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+
+
+# Override the default database with the one specified in DATABASE_URL, if provided
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
+
+
+# print("DATABASE_URL:", os.environ.get("DATABASE_URL"))
 
 
 # Password validation
